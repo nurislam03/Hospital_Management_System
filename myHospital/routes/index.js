64ce5caa290f2980367e5ca6983153282ab87c8b
Nur Islam <nurislam0333@gmail.com>
2018-04-04 20:27:35 +0600

    doctor registration | doctor profile added

--- myHospital/routes/index.js
+++ myHospital/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'myHospital' });
 });
 
+router.get('/doctor-registration', function(req, res, next) {
+  res.render('doctors/doctorRegistration');
+});
+
 module.exports = router;

20a7285593687064a8908099d4286b877aad79dc
Nur Islam <nurislam0333@gmail.com>
2018-04-07 00:57:45 +0600

    patient registration/profile updated | necessary page added

--- myHospital/routes/index.js
+++ myHospital/routes/index.js
@@ -6,15 +6,45 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'TBA-Hospital' });
 });
 
-router.get('/patient-admission', function(req, res, next) {
+/* GET add-patient Form. */
+router.get('/add-patient', function(req, res, next) {
   res.render('patients/patientAdmissionForm');
 });
 
-router.get('/doctor-registration', function(req, res, next) {
-  res.render('doctors/doctorRegistration');
+/* GET add-doctor Form. */
+router.get('/add-doctor', function(req, res, next) {
+  res.render('doctors/doctorInformation');
 });
+
+/* GET doctor-profile page. */
 router.get('/doctor-profile', function(req, res, next) {
   res.render('doctors/doctorProfile');
+});
+
+/* GET patient-profile page. */
+router.get('/patient-profile', function(req, res, next) {
+  res.render('patients/patientProfile');
+});
+
+/* GET patient-Initial Investigation Form. */
+router.get('/patient-investigation', function(req, res, next) {
+  res.render('patients/patientInitialInvestigationForm');
+});
+
+
+/* GET Nurse Entry Form. */
+router.get('/add-nurse', function(req, res, next) {
+  res.render('nurses/nurseInformationForm');
+});
+
+/* GET Ward Information Form. */
+router.get('/add-ward', function(req, res, next) {
+  res.render('wards/wardInformationform');
+});
+
+/* GET Medicine Entry Form. */
+router.get('/add-medicine', function(req, res, next) {
+  res.render('medicine/medicineEntryForm');
 });
 
 module.exports = router;

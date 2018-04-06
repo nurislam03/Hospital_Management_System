e56bac2e60b510c21de1fc412c257e45dbb47423
Nur Islam <nurislam0333@gmail.com>
2018-04-06 15:50:19 +0600

    doctor's page | patient's page added

--- myHospital/routes/index.js
+++ myHospital/routes/index.js
@@ -3,11 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'myHospital' });
+  res.render('index', { title: 'TBA-Hospital' });
+});
+
+router.get('/patient-admission', function(req, res, next) {
+  res.render('patients/patientAdmissionForm');
 });
 
 router.get('/doctor-registration', function(req, res, next) {
   res.render('doctors/doctorRegistration');
+});
+router.get('/doctor-profile', function(req, res, next) {
+  res.render('doctors/doctorProfile');
 });
 
 module.exports = router;
